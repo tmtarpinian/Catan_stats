4edb623484f57e79e1963a069ae01e70cdde2421
ferrisbueller66 <github email address>
2020-05-13 14:33:48 -0400

    add authentication to turns controller

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -1,37 +1,45 @@
 class TurnsController < ApplicationController
 
-    
-
-    get '/games/:id/turns/new' do         #new
-        #@game = current_user.games.find_by_id(params[:id])
-        #binding.pry
-        #erb :"/turns/new"
-    end
-
     post '/games/:id/turns' do         #create
-        @game = current_user.games.find_by_id(params[:id])
-        @turn = @game.turns.build(result: params[:result])   
-        @turn.save                            
-        redirect "/games/#{@game.id}"
+        if logged_in?
+            @game = current_user.games.find_by_id(params[:id])
+            @turn = @game.turns.build(result: params[:result])   
+            @turn.save                            
+            redirect "/games/#{@game.id}"
+        else
+            redirect '/login'
+        end
     end
 
     get '/games/:game_id/turns/:id/edit' do         #edit
-        @game = current_user.games.find_by_id(params[:game_id])
-        @turn = @game.turns.find_by_id(params[:id])
-        erb :'turns/edit'
+        if logged_in?
+            @game = current_user.games.find_by_id(params[:game_id])
+            @turn = @game.turns.find_by_id(params[:id])
+            erb :'turns/edit'
+        else
+            redirect '/login'
+        end
     end
         
     patch '/games/:game_id/turns/:id' do            #update
-        @game = current_user.games.find_by_id(params[:game_id])
-        @turn = @game.turns.find_by_id(params[:id])
-        @turn.update(result: params[:result])
-        redirect "/games/#{@game.id}"                           
+        if logged_in?
+            @game = current_user.games.find_by_id(params[:game_id])
+            @turn = @game.turns.find_by_id(params[:id])
+            @turn.update(result: params[:result])
+            redirect "/games/#{@game.id}"
+        else
+            redirect '/login'
+        end                       
     end
 
     delete '/games/:game_id/turns/:id' do          #delete
-        @game = current_user.games.find_by_id(params[:game_id])
-        @turn = @game.turns.find_by_id(params[:id])
-        @turn.destroy
-        redirect "/games/#{@game.id}"                        
+        if logged_in?
+            @game = current_user.games.find_by_id(params[:game_id])
+            @turn = @game.turns.find_by_id(params[:id])
+            @turn.destroy
+            redirect "/games/#{@game.id}"
+        else
+            redirect '/login'
+        end                     
     end
 end
