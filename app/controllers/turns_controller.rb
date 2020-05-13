e5cabdfdfaee3b1dd3c139e05e75ba682031bd3d
ferrisbueller66 <github email address>
2020-05-13 13:08:52 -0400

    add delete action to turns controller

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -20,18 +20,19 @@ class TurnsController < ApplicationController
         @turn = @game.turns.find_by_id(params[:id])
         erb :'turns/edit'
     end
-
-    patch '/games/:game_id/turns/:id/' do            #update
+        
+    patch '/games/:game_id/turns/:id' do            #update
         binding.pry
-        @turn = Turn.find_by_id(params[:id])
+        @game = current_user.games.find_by_id(params[:game_id])
+        @turn = @game.turns.find_by_id(params[:id])
         @turn.update(result: params[:result])
-        redirect '/users'                               ###NEED TO ROUTE BACK TO GAME ID PAGE
+        redirect "/games/#{@game.id}"                           
     end
 
-    delete 'games/:game_id/turns/:id/' do          #delete
-        #@turn = Turn.find_by_id(params[:id])
-        #params
-        #@turn.destroy
-        #redirect '/users'                         
+    delete '/games/:game_id/turns/:id' do          #delete
+        @game = current_user.games.find_by_id(params[:game_id])
+        @turn = @game.turns.find_by_id(params[:id])
+        @turn.destroy
+        redirect "/games/#{@game.id}"                        
     end
 end
