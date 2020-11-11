92661b1ecc95e76c9bfa7ff043835e299ce76ffd
tmtarpinian <tmtarpinian@gmail.com>
2020-11-11 15:39:10 -0500

    delete line 1 of Rakefile (redundant), uncomment lines 25-36 of environment.rb to configure postgresql production db; rollback migrations and re-migrate

--- config/environment.rb
+++ config/environment.rb
@@ -13,23 +13,26 @@ end
 configure :development do
   ENV['SINATRA_ENV'] ||= "development"
 
+  require 'bundler/setup'
+  Bundler.require(:default, ENV['SINATRA_ENV'])
+
   ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
   )
 end
 
-# configure :production do
-# 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/catan')
+configure :production do
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/catan')
 
-# 	ActiveRecord::Base.establish_connection(
-# 			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-# 			:host     => db.host,
-# 			:username => db.user,
-# 			:password => db.password,
-# 			:database => db.path[1..-1],
-# 			:encoding => 'unicode'
-# 	)
-# end
+	ActiveRecord::Base.establish_connection(
+			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
+end
 
 require_all 'app'
