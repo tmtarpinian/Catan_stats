bc18ee68bd6bc7bf6771788662d1272546abbed4
tmtarpinian <tmtarpinian@gmail.com>
2020-11-27 23:37:58 -0500

    write unit tests ins spec directory for User model

--- spec/models/user.spec.rb
+++
@@ -1,8 +0,0 @@
-describe 'class User' do
-    let(:user) { FactoryBot.create(:user) }
-#   create_table "users", force: :cascade do |t|
-#     t.string "name"
-#     t.string "email"
-#     t.string "password_digest"
-#   end
-end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+RSpec.describe User, type: :model do
+	let(:user1){
+			User.create(name: "Dirty Harry", email: "email@email.com", password: "password")
+	}
+	let(:user2){
+			User.new(name: "Dirty Harry", email: "email@email.com", password: "password")
+    }
+    let(:game){
+        Game.new(name: "Catan", number_of_players: 3)
+    }
+
+	context "validations" do 
+		it "is invalid without a name" do 
+			expect(User.create(name: nil, email: "email@email.com", password: "password")).to_not be_valid
+		end
+		it "is invalid without an email" do 
+			expect(User.create(name: "Dirty Harry", email: nil, password: "password")).to_not be_valid
+		end
+		it "is invalid without a unique email" do 
+				expect(user2).to_not be_valid
+			end
+		it "is invalid without a password" do 
+			expect(User.create(name: "Dirty Harry", email: "email@email.com", password: nil)).to_not be_valid
+		end
+	end
+
+  context "attributes" do
+    it "has a name" do 
+      expect(user1.name).to eq("Dirty Harry")
+    end 
+
+    it "has an email" do 
+      expect(user1.email).to eq("email@email.com")
+    end
+
+    it "has a password" do 
+      expect(user1.password).to eq("password")
+    end
+	end
+
+	context "associations" do
+		it "has many games" do
+			user1.games << game
+			expect(user1.games).to include(game)
+		end
+  end
+end
