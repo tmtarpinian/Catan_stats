86da79cdcf418d1bb51b5a5d3ab345b16afbb77b
ferrisbueller66 <tmtarpinian@gmail.com>
2020-12-27 16:48:46 -0500

    add third test to sessions controller spec login context

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper'
 
 describe 'Sessions Controller', type: :feature do
-	it "renders login page successfully" do
-		visit '/login'
-		expect(page.status_code).to eq(200)
-    end
+    context "/login controller action" do
+        it "renders login page successfully" do
+            visit '/login'
+            expect(page.status_code).to eq(200)
+        end
 
-    it 'successful login redirects to user profile page' do
-        User.create(:name => "wilfred", :email => "wilfred@wilfred.com", :password => "wilfred")
-        visit '/login'
-        fill_in(:email, :with => "wilfred@wilfred.com")
-        fill_in(:password, :with => "wilfred")
-        click_button 'submit'
-        visit '/profile'
-        expect(current_path).to eq('/profile')
-        expect(page).to have_content("wilfred")
+        it 'successful login redirects to user profile page' do
+            User.create(:name => "wilfred", :email => "wilfred@wilfred.com", :password => "wilfred")
+            visit '/login'
+            fill_in(:email, :with => "wilfred@wilfred.com")
+            fill_in(:password, :with => "wilfred")
+            click_button 'submit'
+            visit '/profile'
+            expect(current_path).to eq('/profile')
+            expect(page).to have_content("wilfred")
+        end
+
+        it 'redirects unsuccessful logins back to login page' do
+            visit '/login'
+            fill_in(:email, :with => "")
+            fill_in(:password, :with => "")
+            click_button 'submit'
+            expect(current_path).to eq('/login')
+        end
     end
 end
