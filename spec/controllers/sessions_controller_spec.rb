4f6c7beefbcad254c5f6c17cf8aa03c78bca4145
ferrisbueller66 <tmtarpinian@gmail.com>
2020-12-27 18:07:17 -0500

    add first two unit tests in sessions controller spec for logout action;  begin writing 3rd test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -26,4 +26,56 @@ describe 'Sessions Controller', type: :feature do
             expect(current_path).to eq('/login')
         end
     end
+
+    context "/logout controller action" do
+        it 'successfully logs out a logged-in user' do
+            User.create(:name => "wilfred", :email => "wilfred@wilfred.com", :password => "wilfred")        
+            visit '/login'
+            fill_in(:email, :with => "wilfred@wilfred.com")
+            fill_in(:password, :with => "wilfred")
+            click_button 'submit'
+            visit '/profile'
+            
+            expect(session[:id]).to eq(1)
+            expect(current_path).to eq('/profile')
+            expect(page).to have_content("wilfred")
+            visit '/logout'
+            expect(current_path).to eq('/')
+        end
+
+        it 'redirects the homepage after logout' do
+            User.create(:name => "wilfred", :email => "wilfred@wilfred.com", :password => "wilfred")
+            visit '/login'
+            fill_in(:email, :with => "wilfred@wilfred.com")
+            fill_in(:password, :with => "wilfred")
+            click_button 'submit'
+            visit '/profile'
+            expect(current_path).to eq('/profile')
+            expect(page).to have_content("wilfred")
+            visit '/logout'
+            expect(current_path).to eq('/')
+        end
+
+        it 'redirects anyone other than a logged-in user to the homepage' do
+            visit '/logout'
+            expect(current_path).to eq('/')
+        end
+
+        it 'access to site requires authentication after /logout' do
+            visit '/logout'
+            expect(current_path).to eq('/')
+            visit '/profile'
+            expect(current_path).to eq('/login')
+        end
+    end
+end
+
+
+get '/logout' do
+    if logged_in?
+        session.clear
+        redirect '/'
+    else
+        redirect '/'
+    end
 end
