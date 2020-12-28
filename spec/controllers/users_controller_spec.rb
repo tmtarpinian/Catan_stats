3c279e9792241a07927a7b7c8b524cfe328f66d9
ferrisbueller66 <tmtarpinian@gmail.com>
2020-12-28 12:59:16 -0500

    add unit tests for users edit/update actions in users controllers spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,13 +1,22 @@
 require 'spec_helper'
 
 describe 'User Controller', type: :feature do
+
+	def user_signup
+		fill_in(:name, :with => "wilfred")
+		fill_in(:email, :with => "wilfred@wilfred.com")
+		fill_in(:password, :with => "wilfred")
+		click_button 'submit'
+	end
+
+	def user_signin
+		fill_in(:email, :with => "wilfred@wilfred.com")
+		fill_in(:password, :with => "wilfred")
+		click_button 'submit'
+	end
+
 	context "/signup action" do
-		def user_signup
-			fill_in(:name, :with => "wilfred")
-			fill_in(:email, :with => "wilfred@wilfred.com")
-			fill_in(:password, :with => "wilfred")
-			click_button 'submit'
-		end
+	
 
 		it "renders signup page successfully" do
 			visit '/signup'
@@ -25,9 +34,40 @@ describe 'User Controller', type: :feature do
 		it 'unsuccessful signup re-renders signup page' do
 			visit '/signup'
 			expect(current_path).to eq('/signup')
-			user_signup
-			expect(current_path).to eq('/profile')
-			expect(page).to have_content("wilfred")
+			fill_in(:name, :with => "")
+			click_button 'submit'
+			expect(current_path).to eq('/signup')
+			expect(page).to have_content("Please Sign Up Below")
 		end
-	do
+	end
+
+	context "/users edit action" do
+		it 'only authenticated users can edit a user' do
+			visit '/users/edit'
+			expect(current_path).to eq('/login')
+		end
+
+		it 'authenticated users can access /users/edit' do
+			visit '/signup'
+			user_signup
+			visit '/users/edit'
+			expect(current_path).to eq('/users/edit')
+			expect(page).to have_content("Edit Your Profile Here:")
+		end
+
+		it 'submitting edit user form redirects to /profile' do
+			visit '/signup'
+			user_signup
+			visit '/users/edit'
+			fill_in(:name, :with => "wolfy")
+			fill_in(:email, :with => "wilfred@wilfred.com")
+			click_button 'submit'
+			expect(current_path).to eq('/profile')
+			expect(page).to have_content("wolfy")
+		end
+		
+	end
+
+	context "/users delete action" do
+	end
 end
