c7007d699c699888a091c8b8d831410281b855f7
ferrisbueller66 <tmtarpinian@gmail.com>
2020-12-28 13:57:16 -0500

    add unit test for edit form to user features spec

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -18,4 +18,36 @@ describe 'User Features' do
 			expect(User.first.name).to eq("wilfred")
 		end
 	end
+
+	context "Edit" do
+		def user_signup
+			fill_in(:name, :with => "wilfred")
+			fill_in(:email, :with => "wilfred@wilfred.com")
+			fill_in(:password, :with => "wilfred")
+			click_button 'submit'
+		end
+
+		it 'user edit form renders on /users/edit ' do
+			visit '/signup'
+			user_signup
+			name_input = "<input type='text' name='name' value='#{User.first.name}'>"
+			email_input = "<input type='text' name='email' value='#{User.first.email}'>"
+			visit '/users/edit'
+			expect(current_path).to eq('/users/edit')
+			expect(find_field('name').value).to eq "#{User.first.name}"
+			expect(find_field('email').value).to eq "#{User.first.email}"
+		end
+
+		it 'submitted edit user form updates user ' do
+			visit '/signup'
+			user_signup
+			visit '/users/edit'
+			fill_in(:name, :with => "wolfy")
+			fill_in(:email, :with => "wilfred@wilfred.com")
+			click_button 'submit'
+			expect(current_path).to eq('/profile')
+			expect(page).to have_content("wolfy")
+			expect(User.first.name).to eq("wolfy")
+		end
+	end
 end
