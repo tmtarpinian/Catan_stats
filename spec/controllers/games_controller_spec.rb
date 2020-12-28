2dbf9e2ce368655e44fb19f1483f713df0366797
ferrisbueller66 <tmtarpinian@gmail.com>
2020-12-28 16:55:58 -0500

    write unit tests for games show action in games controller spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -13,8 +13,6 @@ describe "Games Controller" do
 	# Turn.create(game_id: Game.first.id, result: 4)
 	# Turn.create(game_id: Game.first.id, result: 7)
 
- 
-
 	describe "/games index action" do
 
 		it '/games responds with a 200 status code' do
@@ -36,12 +34,33 @@ describe "Games Controller" do
 			visit '/games'
 			expect(current_path).to eq('/login')
 		end
+	end
 
-		# move to features
-		# it "displays a list of games" do
-		# 	expect(page.body).to include(game_name)
-		# 	expect(page.body).to include(@game_2.name)
-		# end
+	describe "/games/:id show action" do
+
+		it '/games/:id responds with a 200 status code' do
+			visit '/signup'
+			user_signup
+			Game.create(user_id: User.first.id, number_of_players: 4, name: "Catan")
+			visit '/games/1'
+			expect(page.status_code).to eq(200)
+		end
+
+		it 'authenticated users can access an individual game' do
+			visit '/signup'
+			user_signup
+			Game.create(user_id: User.first.id, number_of_players: 4, name: "Catan")
+			visit '/games/1'
+			expect(current_path).to eq('/games/1')
+			expect(page).to have_content("Game: #{Game.first.name}")
+		end
+
+		it 'only authenticated users can access an individual game' do
+			visit '/games/1'
+			expect(current_path).to eq('/login')
+		end
+
+	
 
 		# visit '/signup'
 		# visit '/signup'
@@ -70,35 +89,7 @@ describe "Games Controller" do
 		# end
 	end
 
-	describe "show page '/games/:id'" do
-    before do
-      visit "/games/#{@game_1.id}"
-    end
 
-    it 'responds with a 200 status code' do
-      expect(page.status_code).to eq(200)
-    end
-
-    it "displays the game's name" do
-      expect(page.body).to include(@game_1.name)
-    end
-
-    it "displays the game's date" do
-      expect(page.body).to include(@game_1.gamedate.to_s)
-    end
-
-    it "displays the game's status" do
-      expect(page.body).to include(@game_1.status)
-		end
-		
-		# it "displays the game's rolls" do
-		# 		# 	all_link_hrefs = page.all(:css, "a[href]").map do |element| 
-		# # 		binding.pry
-		# # 		element[:href] 
-		# # 	end
-		# # 	expect(all_link_hrefs).to include("/games/#{@game1.id}")
-		# # 	expect(all_link_hrefs).to include("/games/#{@game2.id}")
-    # end
 
     # it "contains a form to delete the game" do
     #   expect(page.find(:css, "form")[:action]).to eq("/games/#{@game_1.id}")
