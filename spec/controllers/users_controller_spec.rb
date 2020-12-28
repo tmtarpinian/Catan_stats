8f0aaa5f52fca8f856995ed3fbdd9a7cbab5523b
ferrisbueller66 <tmtarpinian@gmail.com>
2020-12-28 15:06:36 -0500

    begin writing unit tests for delete action in users controller spec; delete actions hould delete a user and all their respective games and turns

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -84,5 +84,51 @@ describe 'User Controller', type: :feature do
 	end
 
 	context "/users delete action" do
+		it 'delete action deletes an authenticated user' do
+			visit '/signup'
+			user_signup
+			visit '/users/edit'
+			click_button 'Delete User'
+      expect(page.status_code).to eq(200)
+			expect(User.count).to eq(0)
+		end
+
+		it 'delete action deletes the games and turns of an authenticated user' do
+			visit '/signup'
+			visit '/signup'
+			user_signup
+			Game.create(user_id: User.first.id, number_of_players: 4, name: "Catan")
+			Turn.create(game_id: Game.first.id, result: 4)
+			Turn.create(game_id: Game.first.id, result: 7)
+			expect(User.count).to eq(1)
+			expect(Game.count).to eq(1)
+			expect(Turn.count).to eq(2)
+			visit '/users/edit'
+			click_button 'Delete User'
+      expect(page.status_code).to eq(200)
+			expect(User.count).to eq(0)
+			expect(Game.count).to eq(0)
+			expect(Turn.count).to eq(0)
+		end
+
+		it 'delete action redirects to root route for authenticated users' do
+			visit '/signup'
+			user_signup
+			visit '/users/delete'
+			fill_in(:name, :with => "")
+			fill_in(:email, :with => "wilfred@wilfred.com")
+			click_button 'submit'
+			expect(current_path).to eq('/users/edit')
+		end
+
+		it 'delete action redirects to root route for authenticated users' do
+			visit '/signup'
+			user_signup
+			visit '/users/delete'
+			fill_in(:name, :with => "")
+			fill_in(:email, :with => "wilfred@wilfred.com")
+			click_button 'submit'
+			expect(current_path).to eq('/users/edit')
+		end
 	end
 end
