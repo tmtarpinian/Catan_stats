d1df963306b293e280f409a5b2495c18630e4c42
ferrisbueller66 <tmtarpinian@gmail.com>
2020-12-28 15:10:50 -0500

    add dependent: :destroy to user and game models to destroy dependent table data upon deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
-    has_many :games
-    has_many :turns, through: :games
+    has_many :games, dependent: :destroy
+    has_many :turns, through: :games, dependent: :destroy
 
     validates :name, :email, presence: true
     validates :email, uniqueness: true
