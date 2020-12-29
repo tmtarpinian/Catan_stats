657ee77b01fe092fe111fbdfb7e957db09f849e9
ferrisbueller66 <tmtarpinian@gmail.com>
2020-12-29 17:59:39 -0500

    aupdate id name for delete turn buttonn in game show view; write fthird unit tests for turns controller spec

--- spec/controllers/turns_controller_spec.rb
+++ spec/controllers/turns_controller_spec.rb
@@ -38,9 +38,21 @@ describe "Turns Controller" do
 			Turn.create(:result => 7, :game_id => Game.first.id)
 			expect(Turn.count).to eq(2)
 			visit '/games/1'
-			find('#roll-1').click
+			find('#delete-roll-1').click
 			expect(Turn.count).to eq(1)
 			expect(Turn.first.result).to eq(7)
+		end
+
+		it 'redirects to game show page after delating a turn for a game instance' do
+			visit '/signup'
+			user_signup
+			Game.create(user_id: User.first.id, number_of_players: 4, name: "Catan")
+			Turn.create(:result => 5, :game_id => Game.first.id)
+			Turn.create(:result => 7, :game_id => Game.first.id)
+			expect(Turn.count).to eq(2)
+			visit '/games/1'
+			find('#delete-roll-1').click
+			expect(current_path).to eq("/games/#{Game.first.id}")
 		end
 	end
 end
